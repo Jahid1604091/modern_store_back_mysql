92ff01174ba298436eaacbb9a2f1b2894fc82cd2
jahid1604091 <jh409780@gmail.com>
2025-10-26 18:30:43 +0600

    dtos add

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,62 @@
 const express = require('express');
-const { getProfile, login, register } = require('../controllers/userController.js');
-const { protect } = require('../middleware/authMiddleware.js');
+const { getProfile, login, register, getAllPermissions, createPermission, deletPermissionPermanently, getAllRoles, createRole, editRole, deleteRolePermanently } = require('../controllers/userController.js');
+const { protect, authorize } = require('../middleware/authMiddleware.js');
+const loginValidationRules = require('../dtos/loginDto.js');
+const { createPermissiomValidationRules, deletePermissionValidationRules } = require('../dtos/permissionDto.js');
+const { createRoleValidationRules, deleteRoleValidationRules } = require('../dtos/roleDto.js');
+const validator = require('../middleware/validator');
 
 const router = express.Router();
 
 router.route('/register').post(register)
-router.route('/login').post(login)
 router.route('/profile').get(protect, getProfile)
+/* -------------------- AUTH -------------------- */
+router.post('/login', loginValidationRules(), validator, login);
+
+/* -------------------- PERMISSIONS -------------------- */
+router.get('/permissions', protect, authorize('admin', 'super-admin'), getAllPermissions);
+router.post(
+  '/permissions',
+  protect,
+  authorize('admin', 'super-admin'),
+  createPermissiomValidationRules(),
+  validator,
+  createPermission
+);
+router.delete(
+  '/permissions/:id/delete',
+  protect,
+  authorize('admin', 'super-admin'),
+  deletePermissionValidationRules(),
+  validator,
+  deletPermissionPermanently
+);
+
+/* -------------------- ROLES -------------------- */
+router.get('/roles', protect, getAllRoles);
+router.post(
+  '/roles',
+  protect,
+  authorize('admin', 'super-admin'),
+  createRoleValidationRules(),
+  validator,
+  createRole
+);
+router.patch(
+  '/roles/:id',
+  protect,
+  authorize('admin', 'super-admin'),
+  deleteRoleValidationRules(),
+  validator,
+  editRole
+);
+router.delete(
+  '/roles/:id/delete',
+  protect,
+  authorize('admin', 'super-admin'),
+  deleteRoleValidationRules(),
+  validator,
+  deleteRolePermanently
+);
 
 module.exports = router;
