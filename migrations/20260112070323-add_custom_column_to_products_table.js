7cc9635d5add11f4418a6bc864713fd357dbcee1
jahid.modernweb <jh409780@gmail.com>
2026-01-25 20:59:00 +0600

    product table renamed

--- migrations/20260112070323-add_custom_column_to_products_table.js
+++ migrations/20260112070323-add_custom_column_to_products_table.js
@@ -2,28 +2,28 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('products', 'min_stock_threshold', {
+    await queryInterface.addColumn('Products', 'min_stock_threshold', {
       type: Sequelize.INTEGER,
       allowNull: true,
       defaultValue:1,
       after:'stock_quantity'
     });
 
-    await queryInterface.addColumn('products', 'barcode', {
+    await queryInterface.addColumn('Products', 'barcode', {
       type: Sequelize.STRING,
       allowNull: true,
       after:'id'
     });
 
-    await queryInterface.addColumn('products', 'metadata', {
+    await queryInterface.addColumn('Products', 'metadata', {
       type: Sequelize.JSON,
       allowNull: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('products', 'min_stock_threshold');
-    await queryInterface.removeColumn('products', 'barcode');
-    await queryInterface.removeColumn('products', 'metadata');
+    await queryInterface.removeColumn('Products', 'min_stock_threshold');
+    await queryInterface.removeColumn('Products', 'barcode');
+    await queryInterface.removeColumn('Products', 'metadata');
   }
 };
