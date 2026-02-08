4a2bde800cfe2b9f4061bd3191c51f7057d0bb4c
jahid.modernweb <jh409780@gmail.com>
2026-02-08 23:18:24 +0600

    port updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,17 +8,6 @@ module.exports = {
       watch: false,                  // turn on only for development
       autorestart: true,
       max_memory_restart: "500M",
-
-      env: {
-        NODE_ENV: "development",
-        PORT: 3000
-      },
-
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000
-      },
-
       log_date_format: "YYYY-MM-DD HH:mm:ss",
       error_file: "./logs/error.log",
       out_file: "./logs/out.log",
